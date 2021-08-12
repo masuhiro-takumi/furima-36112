ac6f9ef89d911da1a4470345d43f2aba4bddaca2
masupin <86293616+masuhiro-takumi@users.noreply.github.com>
2021-08-12 22:41:24 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, except:  [:index, :show]
+  before_action :move_to_index, except:  [:index, :show, :new, :create]
   before_action :item_presence, only: [:edit, :update, :destory]
 
 
